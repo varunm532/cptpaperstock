4f995b65049192923096d879ec5e331dc44e449a
TDWolff <torin.dean.wolff@gmail.com>
2024-01-30 08:17:21 -0800

    Modifications to Config.js and worked on signin.html

--- static/js/api/config.js
+++ static/js/api/config.js
@@ -1,7 +1,7 @@
 export var uri;
 if (location.hostname === "localhost") {
-        uri = "http://localhost:8086";
-} else if (location.hostname === "127.0.0.1") {
+        uri = "http://127.0.0.1:8086";
+if (location.hostname === "127.0.0.1") {
         uri = "http://127.0.0.1:8086";
 } else {
         uri = "https://flask2.nighthawkcodingsociety.com";
